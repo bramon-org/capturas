163dcd5f905bf6a02be3f053fa4625270451e639
Thiago Paes <mrprompt@gmail.com>
2021-01-19 19:09:54 -0300

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
     <link rel="stylesheet" href="/css/lightbox.css" />
     <link rel="stylesheet" href="/css/app.css" />
+
+    <link rel="shortcut icon" href="/favicon.ico" />
 </head>
 <body>
     <div id="app">
